393655d290785cb91a520d2aaf4fb228d9c4c81d
栗岡智己 <kuritomo0512@gmail.com>
2018-08-26 13:36:51 +0900

    lineBot started

--- main.go
+++ main.go
@@ -1,35 +1,65 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+    "log"
+    "net/http"
+    "os"
+    "fmt"
+	"github.com/gin-gonic/gin"
+    "github.com/line/line-bot-sdk-go/linebot"  // ① SDKを追加
 )
 
-func determineListenAddress() (string, error) {
-	port := os.Getenv("PORT")
-	if port == "" {
-		return "", fmt.Errorf("$PORT not set")
-	}
-	return ":" + port, nil
-}
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World and kurioka tomoki")
-
-}
-
 func main() {
-	addr, err := determineListenAddress()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// 環境変数取得　heroku config:setで設定済み
+    port := os.Getenv("PORT")
 
-	http.HandleFunc("/", hello)
-	log.Printf("Listening on %s...\n", addr)
+    if port == "" {
+        log.Fatal("$PORT must be set")
+    }
+    // ② LINE bot instanceの作成
+    bot, err := linebot.New(
+        os.Getenv("CHANNEL_SECRET"),
+        os.Getenv("CHANNEL_TOKEN"),
+    )
+    if err != nil {
+        log.Fatal(err)
+    }
+    router := gin.New()
+    router.Use(gin.Logger())
+    router.LoadHTMLGlob("templates/*.tmpl.html")
+    router.Static("/static", "static")
 
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		panic(err)
-	}
+
+    router.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.tmpl.html", nil)
+    })
+
+    // ③ LINE Messaging API用の Routing設定
+    router.POST("/callback", func(c *gin.Context) {
+        fmt.Println("got a message")
+        events, err := bot.ParseRequest(c.Request)
+        if err != nil {
+            if err == linebot.ErrInvalidSignature {
+                log.Print(err)
+            }
+            return
+        }
+
+        // event にはidexが入るので、値は_に詰める
+        for _, event := range events {
+            // event setting
+            if event.Type == linebot.EventTypeMessage {
+                switch message := event.Message.(type) {
+                // メッセージがText stringで入ってくる場合
+                case *linebot.TextMessage:
+                    // DO. で　http.Response型のポインタ（とerror）が返ってくる
+                    if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+                        log.Print(err)
+                    }
+                }
+            }
+        }
+    })
+
+    router.Run(":" + port)
 }
