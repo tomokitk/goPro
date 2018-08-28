42bed1bc71966ecaee2c26f6853ffd517a729d86
栗岡智己 <kuritomo0512@gmail.com>
2018-08-28 23:58:35 +0900

    ランダム生成

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
     "log"
+    "math/rand"
+    "time"
     "net/http"
     "os"
     "fmt"
 	"github.com/gin-gonic/gin"
     "github.com/line/line-bot-sdk-go/linebot"  // ① SDKを追加
+    // "database/sql"
+	// 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
@@ -16,6 +20,11 @@ func main() {
     if port == "" {
         log.Fatal("$PORT must be set")
     }
+
+    // db, err := sql.Open("mysql", "root:J02M05A004@tcp(127.0.0.1:13306)/gosample" )
+	// if err != nil {
+    //     log.Fatal("DB is not valid")
+    // }
     // ② LINE bot instanceの作成
     bot, err := linebot.New(
         os.Getenv("CHANNEL_SECRET"),
@@ -55,11 +64,47 @@ func main() {
                     // DO. で　http.Response型のポインタ（とerror）が返ってくる
                     // ReplyMessage関数呼ぶ
                     if message.Text == "綺麗系が好き"{
-                        if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text + "\n おゆみちゃんはどうですか？\n\n"+"https://www.instagram.com/youme_mlk/?hl=ja")).Do(); err != nil {
+
+                        beatifulMap := map[int]string{
+                            0:"\n 貴島明日香ちゃんはどうですか？\n\n"+"https://www.instagram.com/asuka_kijima/?hl=ja",
+                            1: "\n おゆみちゃんはどうですか？\n\n"+"https://www.instagram.com/youme_mlk/?hl=ja",
+                            2: "\n あやプーさんはどうですか？\n\n"+"https://www.instagram.com/ayapooh_22/?hl=ja",
+                            3: "\n yuu__aaaちゃんはどうですか？\n\n"+"https://www.instagram.com/yuu__aaa/?hl=ja",
+                        }
+
+                        index := choice(beatifulMap)
+
+                        theBeautiful := beatifulMap[index]
+
+
+                        // db, err := sql.Open("mysql", "root:J02M05A004@tcp(127.0.0.1:13306)/gosample" )
+
+                        // if err != nil {
+                        //     fmt.Println("DB接続エラー")
+                        //     log.Fatal(err)
+                        // }
+                        // rows, err := db.Query("SELECT id, name FROM sample")
+                        // if err != nil {
+                        //     fmt.Println("DBquery")
+                        //     log.Fatal(err)
+                        // }
+
+                        // for rows.Next() {
+                        //     var id int
+                        //     var name string
+                        //     if err := rows.Scan(&id, &name); err != nil {
+                        //         log.Fatal("Data is not correct")
+                        //     }
+                        //     fmt.Println(id, name)
+                        // }
+                        // fmt.Println(rows)
+                        // db.Close()
+
+                        if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(theBeautiful)).Do(); err != nil {
                             log.Print(err)
                         }
                     }else{
-                        if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("まだリコメンドできないよぅううう")).Do(); err != nil {
+                        if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("https://www.instagram.com/kuriokan/")).Do(); err != nil {
                             log.Print(err)
                         }
                     }
@@ -67,6 +112,16 @@ func main() {
             }
         }
     })
-
     router.Run(":" + port)
+}
+
+// 綺麗系ランダム生成
+func choice(beatiful map[int]string) int {
+    // 乱数作成　ここ課題　乱数の作り方
+    rand.Seed(time.Now().UnixNano())
+    i := rand.Intn(len(beatiful))
+    // 乱数確認ログ
+    fmt.Println(i)
+    return i
+
 }
